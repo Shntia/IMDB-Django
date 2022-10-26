a8800d38feddd756aa9674d8d7055a267fe05556
Shantia <shantia.mogheisi@outlook.com>
2022-10-26 21:15:36 +0330

    create models

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    pass
+
+class Movie(models.Model):
+    pass
+
+class MovieCrew(models.Model):
+    pass
+
+class Crew(models.Model):
+    pass
+
+class Role(models.Model):
+    pass
