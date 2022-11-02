1bdc80777560568aac3271bfe657bf7751a61bd9
Shantia <shantia.mogheisi@outlook.com>
2022-11-02 15:21:07 +0330

    UPDATE admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
+from movies.models import Role, Genre, Movie, MovieCrew, Crew
+
 # Register your models here.
+
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    search_fields = ('title', )
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    search_fields = ('title', )
+    list_filter = ('is_valid', )
+
+
+class MovieCrewInline(admin.TabularInline):
+    model = MovieCrew
+    extra = 2
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'release_date', 'is_valid')
+    search_fields = ('title', )
+    list_filter = ('is_valid', )
+    inlines = (MovieCrewInline, )
+
+
+class CrewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'gender', 'is_valid')
+    search_fields = ('first_name', 'last_name', 'gender')
+    list_filter = ('is_valid', )
+
+
+admin.site.register(Role, RoleAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Crew, CrewAdmin)
